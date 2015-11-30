7eeab33d1d7faa9583c4361f9160a278412a06bc
Marco Cedaro <cedmax@users.noreply.github.com>
2015-11-30 11:51:54 +0000

    fixed view path

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ var data = require('./smartcookies.json').cookies;
 var app = express();
 
 app.set('view engine', 'html');
-app.set('layout', 'layout')
-app.enable('view cache')
-app.engine('html', require('hogan-express'))
+app.set('views', __dirname + '/views');
+app.enable('view cache');
+app.engine('html', require('hogan-express'));
 app.use(require('express-autoprefixer')({ browsers: 'last 2 versions', cascade: false })).use(express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
   res.locals = data[Math.floor(Math.random()*data.length)];
-
-
   res.render('index');
 });
 
