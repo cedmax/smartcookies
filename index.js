b3a9f32bbccfc3675888c5ae3cbc426b76a0cc4c
Marco Cedaro <cedmax@users.noreply.github.com>
2015-11-30 11:40:09 +0000

    first draft

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var settings = require('./settings.json');
+var data = require('./smartcookies.json').cookies;
+
+var app = express();
+
+app.set('view engine', 'html');
+app.set('layout', 'layout')
+app.enable('view cache')
+app.engine('html', require('hogan-express'))
+app.use(require('express-autoprefixer')({ browsers: 'last 2 versions', cascade: false })).use(express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
+  res.locals = data[Math.floor(Math.random()*data.length)];
+
+
+  res.render('index');
+});
+
+var server = app.listen(settings.port);
