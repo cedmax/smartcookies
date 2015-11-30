f024dc0a523e18da695c4652fff5f5c91d293a0e
Marco Cedaro <cedmax@users.noreply.github.com>
2015-11-30 12:13:50 +0000

    added .jscsrc and enabled permalinks

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var express = require('express');
-var settings = require('./settings.json');
-var data = require('./smartcookies.json').cookies;
+var express = require( 'express' );
+var settings = require( './settings.json' );
+var data = require( './smartcookies.json' ).cookies;
+var find = require( 'lodash.find' );
 
 var app = express();
 
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.enable('view cache');
-app.engine('html', require('hogan-express'));
-app.use(require('express-autoprefixer')({ browsers: 'last 2 versions', cascade: false })).use(express.static(__dirname + '/assets'));
+app.set( 'view engine', 'html' );
+app.set( 'views', __dirname + '/views' );
+app.enable( 'view cache' );
+app.engine( 'html', require( 'hogan-express' ));
+app.use( require( 'express-autoprefixer' )( { browsers: 'last 2 versions', cascade: false } )).use( express.static( __dirname + '/assets' ));
 
-app.get('/', function (req, res) {
-  res.locals = data[Math.floor(Math.random()*data.length)];
-  res.render('index');
-});
+app.get( '/:pattern?', function ( req, res ) {
+	var pattern;
+	if (( pattern = req.params.pattern )) {
+		res.locals = find( data, function( genius ) {
+			return genius.pattern === pattern;
+		} );
+		res.render( 'index' );
+	} else {
+		res.redirect( '/' + data[Math.floor( Math.random() * data.length )].pattern );
+	}
+} );
 
-var server = app.listen(settings.port);
+var server = app.listen( settings.port );
