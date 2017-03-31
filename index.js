5ca1c54cd8c657b2216af3282cf77196a4bf5693
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-31 15:41:52 +0100

    max age

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ app.set( 'view engine', 'html' );
 app.set( 'views', __dirname + '/views' );
 app.enable( 'view cache' );
 app.engine( 'html', require( 'hogan-express' ));
-app.use( require( 'express-autoprefixer' )( { browsers: 'last 2 versions', cascade: false } )).use( express.static( __dirname + '/assets' ));
+app.use( require( 'express-autoprefixer' )( { browsers: 'last 2 versions', cascade: false } )).use( express.static( __dirname + '/assets', { maxAge: '31 days' }));
 
 function getContent ( data, patterns, reqPattern ) {
-	var obj = JSON.parse( JSON.stringify( data[reqPattern] ));
-	return extend( obj, {
-		pattern: data[reqPattern].pattern || reqPattern,
-		image: data[reqPattern].image || reqPattern,
-		list: patterns.sort().splice( 1 ).map( function( pattern ) {
-			return {
-				pattern: pattern,
-				selected: ( pattern === reqPattern )
-			}
-		} )
-	} );
+  var obj = JSON.parse( JSON.stringify( data[reqPattern] ));
+  return extend( obj, {
+    pattern: data[reqPattern].pattern || reqPattern,
+    image: data[reqPattern].image || reqPattern,
+    list: patterns.sort().splice( 1 ).map( function( pattern ) {
+      return {
+        pattern: pattern,
+        selected: ( pattern === reqPattern )
+      }
+    } )
+  } );
 }
 
 var dataJSon = JSON.stringify( data );
 
 app.get( '/:pattern?', function ( req, res ) {
-	var reqPattern,
-		patterns = Object.keys( data );
-		
-	if (( reqPattern = req.params.pattern )) {
-		if ( !data[reqPattern] ) {
-			reqPattern = '404';
-			res.locals = getContent( data, patterns, reqPattern );
-			res.status( 404 ).render( 'index' );
-		} else {
-			res.locals = getContent( data, patterns, reqPattern );
-			res.locals.json = dataJSon;
-			res.render( 'index' );
-		}
-	} else {
-		patterns.shift();
-		res.redirect( '/' + patterns[Math.floor( Math.random() * patterns.length )] );
-	}
+  var reqPattern,
+    patterns = Object.keys( data );
+    
+  if (( reqPattern = req.params.pattern )) {
+    if ( !data[reqPattern] ) {
+      reqPattern = '404';
+      res.locals = getContent( data, patterns, reqPattern );
+      res.status( 404 ).render( 'index' );
+    } else {
+      res.locals = getContent( data, patterns, reqPattern );
+      res.locals.json = dataJSon;
+      res.render( 'index' );
+    }
+  } else {
+    patterns.shift();
+    res.redirect( '/' + patterns[Math.floor( Math.random() * patterns.length )] );
+  }
 } );
 
 var server = app.listen( settings.port );
